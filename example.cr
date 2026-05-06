24b0c5ab572bcbcdd77080c460bc2afa88a10d0d
Billy.Zheng <vil963@gmail.com>
2026-05-06 13:10:02 +0800

    Added example.cr

---
+++ example.cr
@@ -0,0 +1,83 @@
+require "./src/awesome_print"
+
+class Person
+  def initialize(@name : String, @rank : Int32, @admin : Bool, @very_long_status : String)
+  end
+end
+
+class RecursivePerson
+  property friend : RecursivePerson?
+
+  def initialize(@name : String)
+  end
+end
+
+class ManyFields
+  def initialize(
+    @a : Int32,
+    @b : Int32,
+    @c : Int32,
+    @d : Int32,
+    @e : Int32,
+    @f : Int32,
+    @g : Int32
+  )
+  end
+end
+
+class MixedKey
+  def initialize(@id : Int32)
+  end
+end
+
+struct StructKey
+  def initialize(@x : Int32, @y : Int32)
+  end
+end
+
+numbers = [1, 2, 3]
+long_numbers = [1, 2, 3, 4, 5, 6, 7]
+nested_numbers = [1, [2, 3], 4]
+tuple_values = {1, 2, 3}
+literal_values = [:alpha, nil, true, false]
+profile = {"name" => "Diana", "rank" => 1, "admin" => false}
+mixed_key_profile = {1 => "one", true => "yes", MixedKey.new(7) => "object"}
+composite_key_profile = {
+  {1, 2} => "tuple",
+  ({name: "Diana", rank: 1}) => "named",
+  StructKey.new(3, 4) => "struct",
+}
+user = {
+  name: "Diana",
+  rank: 1,
+  admin: false,
+}
+person = Person.new("Diana", 1, false, "active")
+recursive_person = RecursivePerson.new("Diana")
+recursive_person.friend = recursive_person
+many_fields = ManyFields.new(1, 2, 3, 4, 5, 6, 7)
+
+ap!(numbers)
+ap!(long_numbers, limit: 5)
+ap!(nested_numbers)
+ap!(tuple_values)
+ap!(literal_values)
+ap!(profile)
+ap!(mixed_key_profile)
+ap!(composite_key_profile)
+ap!(user)
+ap!(StructKey.new(3, 4))
+ap!(person)
+ap!(recursive_person)
+ap!(many_fields, limit: 5)
+ap!(numbers.size + user[:rank])
+ap!(person, order: :sorted)
+ap!(numbers, multiline: false)
+
+puts
+puts "-- custom inspector: natural order --"
+puts AwesomePrint::Inspector.new(indent_size: 2, colors_enabled: false, order: :natural).awesome(profile)
+
+puts
+puts "-- custom inspector: sorted order --"
+puts AwesomePrint::Inspector.new(indent_size: 2, colors_enabled: false, order: :sorted).awesome(person)
